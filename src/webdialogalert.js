b7170a4920f9e8c4485b75fb7d13fdc93240267b
Rahul Sharma(Angular) <rahul.sharma.1@netsolutions.com>
2022-10-01 18:09:28 +0530

    css changes for desktop version

--- src/webdialogalert.js
+++ src/webdialogalert.js
@@ -1,75 +1,81 @@
 class RoboMxWebDialogAlert extends HTMLElement {
-    constructor() {
-        super();
-        var dlg = document.createElement("DIV");
-        var shadow = this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+    var div = document.createElement("DIV");
+    var shadow = this.attachShadow({ mode: "open" });
+    var title = this.getAttribute("title") || "Dialog title";
+    var desc = this.getAttribute("description") || "Dialog description";
+    var imgSrc = this.getAttribute("imgSrc") || null;
+    var position = this.getAttribute("position");
+    var dark = this.getAttribute("darkMode") || false;
+    var link = this.getAttribute("link") || "https://example.com";
 
-        var t = this.getAttribute('title') || 'Dialog title';
-        var d = this.getAttribute('description') || 'Dialog description';
-        var i = this.getAttribute('imgSrc') || null;
-        var width = this.getAttribute('imgWidth') || '200px';
-        var height = this.getAttribute('imgHeight') || '200px';
-        var position = this.getAttribute('position');
-        var dark = this.getAttribute('darkMode') || false;
-        var link = this.getAttribute('link') || 'https://example.com';
-
-        dlg.innerHTML = `
+    div.innerHTML =
+      `
     <div class="robomx-card">
-        <img  class="robomx-card-img"  src=` + i + ` alt="" >
+        <img  class="robomx-card-img"  src=` +
+      imgSrc +
+      ` alt="" >
         <div class="robomx-card-content">
-            <h4 class="robomx-title">` + t + `</h4>
-            <h4 class="robomx-subtitle">` + d + `</h4>
-            <div class="robomx-buttons">
-                <button class="robomx-later" onclick="roboMxDialogDismiss()">Later</button>
-                <button class="robomx-visit"><a class="robomx-wba-href" target="_blank" href="` + link + `">Learn More</a></button>
-            </div>
+            <h3 class="robomx-title">` +
+      title +
+      `</h3>
+            <p class="robomx-subtitle">` +
+      desc +
+      `</p>
+      <div class="robomx-buttons">
+      <button class="robomx-later" onclick="roboMxDialogDismiss()">Later</button>
+      <button class="robomx-visit"><a class="robomx-wba-href" target="_blank" href="` +
+      link +
+      `">Learn More</a></button>
+      </div> 
         </div>
+       
 
     </div>
-`;
+    `;
+    // define css for main element
+    this.style.position = "fixed";
 
-        // define css for main element
-        this.style.position = 'fixed';
+    // align element on sheet
+    switch (position) {
+      case "top-left":
+        this.style.top = "0";
+        this.style.left = "0";
+        break;
+      case "top-right":
+        this.style.top = "0";
+        this.style.right = "0";
+        break;
+      case "bottom-right":
+        this.style.bottom = "0";
+        this.style.right = "0";
+        break;
+      case "center":
+        this.style.margin = "auto";
+        this.style.display = "flex";
+        this.style.justifyContent = "center";
+        this.style.width = "100%";
+        break;
+      default:
+        // by default align the element bottom-left
+        this.style.bottom = "0";
+        this.style.left = "0";
+    }
 
-        // align element on sheet
-        switch (position) {
-            case 'top-left':
-                this.style.top = '0';
-                this.style.left = '0';
-                break;
-            case 'top-right':
-                this.style.top = '0';
-                this.style.right = '0';
-                break;
-            case 'bottom-right':
-                this.style.bottom = '0';
-                this.style.right = '0';
-                break;
-            case 'center':
-                this.style.margin = 'auto';
-                this.style.display = 'flex';
-                this.style.justifyContent = 'center';
-                this.style.width = "100%";
-                break;
-            default:
-                // by default align the element bottom-left
-                this.style.bottom = '0';
-                this.style.left = '0';
-        }
+    var style = document.createElement("STYLE");
 
-        var style = document.createElement('STYLE');
+    // set colors
+    var bgClr = this.getAttribute("bgColor") || "#fff";
+    var txtClr = this.getAttribute("txtColor") || "#494848";
+    if (dark == "true") {
+      bgClr = "#202124";
+      txtClr = "#ffffe9";
+      btnClr = "#909090";
+    }
 
-        // set colors
-        var bgClr = this.getAttribute('bgColor') || '#fff'
-        var txtClr = this.getAttribute('txtColor') || '#373737'
-        var btnClr = this.getAttribute('btnColor') || '#6c6b6b'
-        if (dark == 'true') {
-            bgClr = '#212121';
-            txtClr = '#ffffe9';
-            btnClr = '#909090';
-        }
-
-        style.innerHTML = `
+    style.innerHTML =
+      `
 
 :root {
     font-family: 'Roboto', sans-serif;
@@ -82,9 +88,11 @@ button {
     text-decoration: none;
 }
 .robomx-card {
-    background: ` + bgClr + `;
+    background: ` +
+      bgClr +
+      `;
     width: 515px;
-    height: 215px;
+    height: 200px;
     display: flex;
     box-shadow: 0 0px 4px #d3d3d3;
     border-radius: 5px;
@@ -96,50 +104,64 @@ button {
 .robomx-card-img {
     flex: 1;
     padding: 6px;
+    width: 200px;
+    border-radius: 8px
 }
 
 .robomx-card-content {
-    flex: 2;
+    flex: auto;
     padding: 6px;
 }
 
 .robomx-title {
-    font-size: 17px;
+    margin-top: 10px;
+    margin-bottom: 0;
+    color: ${txtClr}
 }
 
 .robomx-subtitle {
-    max-height: 5.2em;
     overflow: hidden;
-    height: 5.2em;
+    height: calc(140px - 20px);
     line-height: 20.2px;
-    color:  ` + txtClr + `;
+    word-break: break-word;
+    color:  ` +
+      txtClr +
+      `;
     font-weight: lighter;
+    margin-top: 12px;
+    margin-bottom: 10px;
 }
 
 .robomx-buttons {
-    display: flex;
-    justify-content: flex-end;
-    margin-right: 17px;
+    position: absolute;
+    bottom: 16px;
+    right: 20px;
+    
 }
 
 .robomx-later {
     background: none;
     border: none;
     margin-right: 16px;
-    font-size: 16px;
+    font-size: 14px;
     letter-spacing: 1px;
+    color:${txtClr}
 }
 
-.robomx-visit {
+.robomx-visit{
     background: none;
     border: none;
-    font-size: 16px;
+    font-size: 14px;
     letter-spacing: 1px;
 }
-
+.robomx-wba-href {
+  color:${txtClr}
+}
 @media only screen and (max-width: 354px) {
     .robomx-card {
-        background: ` + bgClr + `;
+        background: ` +
+      bgClr +
+      `;
         width: 515px;
         height: 215px;
         display: flex;
@@ -170,13 +192,13 @@ button {
         margin-left: -4px;
     }
 }
-@media screen and (-webkit-min-device-pixel-ratio:0){
-  .robomx-card-img {
-    flex: 1;
-    padding: 6px;
-    height: 200px;
-    width: 200px;
-} 
+// @media screen and (-webkit-min-device-pixel-ratio:0){
+//   .robomx-card-img {
+//     flex: 1;
+//     padding: 6px;
+//     height: 200px;
+//     width: 200px;
+// } 
 }
 @-moz-document url-prefix() {
   .robomx-card-img {
@@ -196,7 +218,9 @@ button {
 }
 @media only screen and (max-width: 600px) {
     .robomx-card {
-        background: ` + bgClr + `;
+        background: ` +
+      bgClr +
+      `;
         width: 100%;
         height: 200px;
         display: flex;
@@ -217,7 +241,9 @@ button {
         overflow: hidden;
         height: 4.8em;
         line-height: 1.2;
-        color:  ` + txtClr + `;
+        color:  ` +
+      txtClr +
+      `;
         font-weight: lighter;
     }
     .robomx-buttons {
@@ -249,21 +275,27 @@ button {
 }
 `;
 
-        // set keyframe
-        if (position.includes('right')) {
-            style.innerHTML = style.innerHTML.replace('translate(-1000px)', 'translate(1000px)');
-        }
-        // render the dialog inside the main element
-        if (!sessionStorage.getItem('roboMxWebDialog')) {
-            shadow.appendChild(style);
-            shadow.appendChild(dlg);
-        }
+    // set keyframe
+    if (position.includes("right")) {
+      style.innerHTML = style.innerHTML.replace(
+        "translate(-1000px)",
+        "translate(1000px)"
+      );
     }
+    // render the dialog inside the main element
+    if (!sessionStorage.getItem("roboMxWebDialog")) {
+      shadow.appendChild(style);
+      shadow.appendChild(div);
+    }
+  }
 }
+
+function roboMxWebDialogInit() {}
 
 function roboMxDialogDismiss() {
-    document.getElementsByTagName('robomx-webdialogalert')[0].style.display = "none";
-    sessionStorage.setItem('roboMxWebDialog', false);
+  document.getElementsByTagName("robomx-webdialogalert")[0].style.display =
+    "none";
+  sessionStorage.setItem("roboMxWebDialog", false);
 }
 
-customElements.define('robomx-webdialogalert', RoboMxWebDialogAlert);
+customElements.define("robomx-webdialogalert", RoboMxWebDialogAlert);
